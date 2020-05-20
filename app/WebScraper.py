c08fc865f1d846581f068ec5bdf94d6ab3a0c5df
Kyran <krs19@xtra.co.nz>
2020-05-20 17:10:48 +1200

    begg update

--- app/WebScraper.py
+++ app/WebScraper.py
@@ -1,18 +1,39 @@
+from time import sleep
+
 import requests
 from bs4 import BeautifulSoup
+from app import toaster
 
 
 class WebScraper:
     def __init__(self):
-        pass
+        self.previous_listing_id = None
 
     def run(self):
         print('Webscraper Started')
-        self.getSite()
+        while True:
+            print('Checking for new jobs')
+            self.getSite()
+            sleep(60 * 5)   # 5 mins
 
     def getSite(self):
         r = requests.get(
             'https://www.seek.co.nz/software-developer-jobs/in-Wellington-Central-Wellington?sortmode=ListedDate'
         )
         soup = BeautifulSoup(r.content)
-        print(soup.prettify())
+        for listing in soup.find_all('article'):
+            if listing.get('data-automation') == 'normalJob':
+                if listing.get('data-job-id') != self.previous_listing_id:
+                    print('Job Found. ID: ', listing.get('data-job-id'))
+                    self.previous_listing_id = listing.get('data-job-id')
+                    job_name = listing.get('aria-label')
+
+                    temp = listing.find_all('span')
+                    job_employer = temp[7].span.a.string
+
+                    winToastDisplay(job_name, job_employer)
+                    break
+
+
+def winToastDisplay(job_name, job_employer):
+    toaster.show_toast(job_name, job_employer)
--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
+from win10toast import ToastNotifier
+
+
+class MyToastNotifier(ToastNotifier):
+    def on_destroy(self, hwnd, msg, wparam, lparam):
+        pass
+
+
+toaster = MyToastNotifier()
+
 from app.WebScraper import WebScraper
-
-
 webscraperInstance = WebScraper()
